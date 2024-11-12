5509ae73e03356c55117112c24f14388a1c82b14
Davit Kurdadze <dkurdadze@scroll.eco>
2024-11-13 01:13:16 +0400

    feat: added new assets for bottomtabs

---
+++ src/theme/assets/icons/tab-bar/index.ts
@@ -0,0 +1,4 @@
+export {default as TabIconExplore} from './tab-icon-explore.svg';
+export {default as TabIconStadiums} from './tab-icon-stadiums.svg';
+export {default as TabIconInformation} from './tab-icon-information.svg';
+export {default as TabIconContacts} from './tab-icon-contacts.svg';
