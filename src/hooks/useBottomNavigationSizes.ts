388b17ebc251b002b95502a3365df84d1e344998
Davit Kurdadze <dkurdadze@scroll.eco>
2024-11-13 17:31:25 +0400

    fix: eslint prettier fixes in those components

--- src/hooks/useBottomNavigationSizes.ts
+++ src/hooks/useBottomNavigationSizes.ts
@@ -1,11 +1,11 @@
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const DEFAULT_HORIZONTAL_INSET = 16;
 const DEFAULT_BOTTOM_INSET = 8;
 const BOTTOM_NAVIGATION_HEIGHT = 58;
 
 const useBottomNavigationSizes = () => {
-  const {bottom, left, right} = useSafeAreaInsets();
+  const { bottom, left, right } = useSafeAreaInsets();
 
   return {
     height: BOTTOM_NAVIGATION_HEIGHT,
