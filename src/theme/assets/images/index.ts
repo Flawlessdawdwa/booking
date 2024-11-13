0bf0b62b14effa739f1b07a6cf019ebb48421f72
Davit Kurdadze <dkurdadze@scroll.eco>
2024-11-13 17:30:46 +0400

    feat: added new assets for the home page

---
+++ src/theme/assets/images/index.ts
@@ -0,0 +1,6 @@
+export { default as Hilton } from './hilton.svg';
+export { default as Marriot } from './marriot.svg';
+export { default as Price } from './price.svg';
+export { default as Route } from './route.svg';
+export { default as Star } from './star.svg';
+export { default as Booking } from './bookingcom.svg';
